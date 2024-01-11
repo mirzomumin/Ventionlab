8dc87502eb575bfac4ffec61d210f625cfa94b70
Mirzomumin <mirzomumin@list.ru>
2024-01-11 15:53:56 +0500

    Update js code

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,8 +1,6 @@
 const addCourseBtn = document.querySelector("#addCourse")
 const addCourseModal = new bootstrap.Modal(document.querySelector("#exampleModalCenter"), {})
 
-
-
 addCourseBtn.addEventListener("click", () => {
     addCourseModal.show()
 })
