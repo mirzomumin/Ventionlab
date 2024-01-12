03b4eec6ef7db9398e8511eaa95e0757021e1e85
Mirzomumin <mirzomumin@list.ru>
2024-01-12 11:25:48 +0500

    Update code

--- backend/common/models.py
+++ backend/common/models.py
@@ -6,5 +6,9 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField("Record creation time", auto_now_add=True)
     updated_at = models.DateTimeField("Time of record change", auto_now=True)
 
+
+
+
+
     class Meta:
         abstract = True
