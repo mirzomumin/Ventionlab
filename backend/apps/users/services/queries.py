e04c225f896cd96c21d62894866531b8a2951514
Mirzomumin <mirzomumin@list.ru>
2024-01-12 18:30:55 +0500

    Ignore 'apps' folder and update pre-commit formatter

--- backend/apps/users/services/queries.py
+++
@@ -1,8 +0,0 @@
-from apps.users.models import InternshipGroup
-from django.db.models import Q
-
-
-def build_participants_qs_by_role(internship: InternshipGroup, role: int) -> Q:
-    return Q(internship_participants__internships=internship) & Q(
-        internship_participants__role=role
-    )
