2f12a44c1f5d53c7385f84a46766a37213297619
Mirzomumin <mirzomumin@list.ru>
2024-01-13 20:20:13 +0500

    Update config gile and workflow

--- backend/apps/courses/forms.py
+++ backend/apps/courses/forms.py
@@ -6,11 +6,7 @@ from apps.courses.models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = [
-            "title",
-            "description",
-            "level",
-        ]
+        fields = ["title", "description", "level"]
 
     def __init__(self, *args, **kwargs):
         super(CourseForm, self).__init__(*args, **kwargs)
